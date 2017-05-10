7928b299b9dac8abcf9b5f5123df8815e3199b2a
Patryk Ptasiński <github@ipepe.pl>
2017-05-10 16:43:39 +0200

    Update application_record.rb

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,20 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+#   acts_as_paranoid
+
+  def self.human_enum_name(enum_name, enum_value)
+    I18n.t("activerecord.attributes.#{model_name.i18n_key}.#{enum_name.to_s.pluralize}.#{enum_value}")
+  end
+
+  def human_enum_name(enum_name)
+    I18n.t("activerecord.attributes.#{model_name.i18n_key}.#{enum_name.to_s.pluralize}.#{self.read_attribute(enum_name)}")
+  end
+
+  protected
+  def self.enum_with_string_values(definitions)
+    definitions.keys.each do |k|
+      definitions[k]= definitions[k].each_with_object(Hash.new) {|ct,h|  h[ct] = ct.to_s}
+    end
+    enum(definitions)
+  end
 end
