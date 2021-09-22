bfa98243035fb6b211f7f217acf1fdf4b618fe77
Patryk Ptasinski <patryk@ipepe.pl>
2021-09-22 16:08:58 +0200

    Add devise user connection identification

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,16 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :user_id
+
+    # def connect
+    #   (self.user_id = fetch_user&.id) ||
+    #     reject_unauthorized_connection
+    # end
+    #
+    # protected
+    #
+    # def fetch_user
+    #   User.find_by(id: cookies.signed['user.id']) || env['warden'].user
+    # end
   end
 end
